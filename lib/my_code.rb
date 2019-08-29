bbf199adc43c2816f72ae2ceb73a1033fbca9686
Sarah H <sarahrose.henderson@gmail.com>
2019-08-29 18:56:35 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -21,16 +21,16 @@ end
 
 
 def reduce(array, sv=nil)
-if sv
-  sum = sv
-  i = 0
-else
-  num1 = array[0]
-  i = 1
-end
-whil i < array.length
-sum = yield(num1, array[i])
-i += 1
-end
-sum
+  if sv
+    sum = sv
+    i = 0
+  else
+    sum = array[0]
+    i = 1
+  end
+  while i < array.length
+    sum = yield(sum, array[i])
+    i += 1
+  end 
+  sum
 end
