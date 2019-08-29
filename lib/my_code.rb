6287988e822135c5f3409b230d3fd866578bc82c
Sarah H <sarahrose.henderson@gmail.com>
2019-08-29 19:01:38 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,36 @@
 # Your Code Here
+# def own_map(array)
+#   final_array = []
+#   counter = 0
+#   while counter < array.size do
+#     yield(final_array.push(array[counter] * -1))
+#      counter += 1
+# end
+# final_array{|innards| {innards}}
+# end
+
+def map (array)
+new = []
+counter = 0
+while counter < array.length
+  new.push(yield(array[counter]))
+counter += 1
+end
+new
+end
+
+
+def reduce(array, sv=nil)
+  if sv
+    sum = sv
+    i = 0
+  else
+    sum = array[0]
+    i = 1
+  end
+  while i < array.length
+    sum = yield(sum, array[i])
+    i += 1
+  end
+  sum
+end
