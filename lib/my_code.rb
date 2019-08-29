9c9d03abec3ab951ae98e4f2dc174d0685f78663
Sarah H <sarahrose.henderson@gmail.com>
2019-08-29 18:50:43 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,25 @@
 # Your Code Here
+# def own_map(array)
+#   final_array = []
+#   counter = 0
+#   while counter < array.size do
+#     yield(final_array.push(array[counter] * -1))
+#      counter += 1
+# end
+# final_array{|innards| {innards}}
+# end
+
+def map (array)
+new = []
+counter = 0
+while counter < array.length
+  new.push(yield(array[counter]))
+counter += 1
+end
+new
+end
+
+
+def reduce(array)
+
+end
